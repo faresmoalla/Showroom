55c75441315cefc68069918446ad4fbd1bf55fc5
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-08-30 12:45:18 +0100

    structure

---
+++ src/main/java/tn/showroom/ShowRoomApplication.java
@@ -0,0 +1,13 @@
+package tn.showroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/ooredoo/ShowRoomApplicationTests.java
@@ -0,0 +1,13 @@
+//package tn.ooredoo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShowRoomApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
